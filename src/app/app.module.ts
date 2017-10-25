e3bea685ef99ef0141677999e9aff71677fb7a5b
Brian Craw <briandcraw@gmail.com>
2017-10-24 22:14:43 -0700

    Added components and services for all the api endpoints

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { NgModule} from '@angular/core'
 
 
 import { AppComponent } from './app.component';
-import { CharacterComponent} from './Character/character.component'
+import { CharacterComponent} from './Character/character.component';
+import { PorfileComponent } from './Profile/porfile/porfile.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ItemsComponent } from './items/items.component';
+import { ProfilelistComponent } from './profilelist/profilelist.component';
+import { ProfileListComponent } from './profile-list/profile-list.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharacterComponent
+    CharacterComponent,
+    PorfileComponent,
+    ProfileComponent,
+    ItemsComponent,
+    ProfilelistComponent,
+    ProfileListComponent
   ],
   imports: [
     BrowserModule,
